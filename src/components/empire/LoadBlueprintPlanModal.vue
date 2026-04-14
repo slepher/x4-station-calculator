9cfa06dda289cf95667e91343d30e4882242e7e7
slepher <slepheric@gmail.com>
2026-04-14 23:22:00 +0800

    fix: remove unused computed import in LoadBlueprintPlanModal

--- src/components/empire/LoadBlueprintPlanModal.vue
+++ src/components/empire/LoadBlueprintPlanModal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+
 import { useI18n } from 'vue-i18n'
 import type { SavedModule } from '@/types/x4'
 import { useBlueprintProductionStore } from '@/store/useBlueprintProductionStore'
