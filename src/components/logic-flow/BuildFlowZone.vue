e8c51be039f917f76c05c0a0efdf4720649e4247
slepher <cxyfree@gmail.com>
2026-05-02 19:59:35 +0800

    fix: group as flex-col justify-center so output card stays centered in full group height

--- src/components/logic-flow/BuildFlowZone.vue
+++ src/components/logic-flow/BuildFlowZone.vue
@@ -208,7 +208,7 @@ onUnmounted(() => document.removeEventListener('click', onDocumentClick, true))
       <div
         v-for="group in presenter.buildFlowGroups.value"
         :key="group.groupKey"
-        class="build-flow-group border border-gray-700 rounded p-3"
+        class="build-flow-group border border-gray-700 rounded p-3 flex flex-col justify-center"
       >
         <div class="flex gap-4 items-start">
           <div class="flex flex-col gap-16 shrink-0 ml-[84px]" style="width: 320px">
@@ -270,7 +270,7 @@ onUnmounted(() => document.removeEventListener('click', onDocumentClick, true))
           </div>
           <div
             v-if="group.outputTags.length > 0"
-            class="build-flow-output-card bg-gray-800/60 border border-gray-600 rounded p-2 shrink-0 self-center mr-5"
+            class="build-flow-output-card bg-gray-800/60 border border-gray-600 rounded p-2 shrink-0 mr-5"
             style="width: 160px"
           >
             <div class="text-xs text-gray-300 font-medium mb-2">{{ t('buildFlow.build_flow_output_card_title') }}</div>
