ae97821e39072f2643deb25c4b327de1b2366f93
slepher <cxyfree@gmail.com>
2026-05-02 23:04:00 +0800

    chore: line card width 320 -> 308

--- src/components/logic-flow/BuildFlowZone.vue
+++ src/components/logic-flow/BuildFlowZone.vue
@@ -236,7 +236,7 @@ onUnmounted(() => document.removeEventListener('click', onDocumentClick, true))
         class="build-flow-group border border-gray-700 rounded p-3 flex flex-col justify-center relative"
       >
         <div class="flex items-start">
-          <div class="flex flex-col gap-16 shrink-0 ml-[68px]" style="width: 320px">
+          <div class="flex flex-col gap-16 shrink-0 ml-[68px]" style="width: 308px">
             <div
               v-for="card in sg.lineCards"
               :key="card.groupId"
