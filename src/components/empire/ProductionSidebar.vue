23bf53c0038bccd0d4d519b10bc2b1169a5a1f16
slepher <slepheric@gmail.com>
2026-05-29 19:44:44 +0800

    fix(sidebar): restore jump-to-binding and station-only delete in context menu

--- src/components/empire/ProductionSidebar.vue
+++ src/components/empire/ProductionSidebar.vue
@@ -352,24 +352,36 @@ onUnmounted(() => {
       >
         <div class="menu-header">{{ t('sector.menu_operations') }}</div>
 
-        <template v-if="contextMenuMode === 'full'">
-          <div class="menu-item" @click="doRename">
-            <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
-            <span>{{ t('sector.rename_station') }}</span>
-          </div>
-
-          <div class="menu-item" @click="duplicateStation">
-            <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
-            <span>{{ t('sector.duplicate_station') }}</span>
-          </div>
-
-          <div class="menu-divider"></div>
-        </template>
-
-        <div class="menu-item danger" @click="confirmDelete">
-          <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
-          <span>{{ t('sector.delete_station') }}</span>
+        <div class="menu-item" @click="jumpToBinding">
+          <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+            <path d="M10.5 13.5L13.5 10.5" />
+            <path d="M8.25 15.75a3.182 3.182 0 0 1-4.5 0 3.182 3.182 0 0 1 0-4.5l3-3a3.182 3.182 0 0 1 4.5 0" />
+            <path d="M15.75 8.25a3.182 3.182 0 0 1 4.5 0 3.182 3.182 0 0 1 0 4.5l-3 3a3.182 3.182 0 0 1-4.5 0" />
+          </svg>
+          <span>{{ t('sector.jump_to_binding') }}</span>
         </div>
+
+        <template v-if="menuTabType === 'station'">
+          <template v-if="contextMenuMode === 'full'">
+            <div class="menu-divider"></div>
+            <div class="menu-item" @click="doRename">
+              <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+              <span>{{ t('sector.rename_station') }}</span>
+            </div>
+
+            <div class="menu-item" @click="duplicateStation">
+              <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
+              <span>{{ t('sector.duplicate_station') }}</span>
+            </div>
+
+            <div class="menu-divider"></div>
+          </template>
+
+          <div class="menu-item danger" @click="confirmDelete">
+            <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+            <span>{{ t('sector.delete_station') }}</span>
+          </div>
+        </template>
       </div>
     </Teleport>
 
